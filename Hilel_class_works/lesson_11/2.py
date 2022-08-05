63bcb8289ba6bfa4faf7f4b74930f6a429cf4853
Vinohradov Volodymyr <eXpertt13@gmail.com>
2022-08-05 16:38:37 +0300

    11 Lesson Hilel class work

---
+++ Hilel_class_works/lesson_11/2.py
@@ -0,0 +1,18 @@
+def connect_to_postgres():
+    # NOTE: ...
+    print("PostgreSQL 13.3")
+
+
+def connect_to_mysql():
+    # NOTE: ...
+    print("MySQL 8")
+
+
+def log(func):
+    print("Connected")
+    return func
+
+data = log(connect_to_postgres)
+
+print(data)
+data()
